472b3a4afcf87f9d683e2816c2a2661cd7772be7
DiegoAP5 <213475@ids.upchiapas.edu.mx>
2024-07-21 18:05:08 -0600

    removed dependencies

--- src/main/java/project/UserService/infraestructure/outputs/UserPersistenceAdapter.java
+++ src/main/java/project/UserService/infraestructure/outputs/UserPersistenceAdapter.java
@@ -1,6 +1,5 @@
 package project.UserService.infraestructure.outputs;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
